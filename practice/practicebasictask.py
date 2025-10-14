454d9f053b9d9a7a4bf09213e99c88b0348abcd8
Muhammad <ahmedmithi909@gmail.com>
2025-10-14 15:19:50 +0500

    Added my practicebasictask.py file

---
+++ practice/practicebasictask.py
@@ -0,0 +1,10 @@
+# ----------------------------------------
+# Assignment: Basic Python Data Types & Input
+#taking input from user:
+name = input('enter your name :') # name input  
+age = input("enter your age:")    # age  input
+hobby=str(input("enter your favourite hobby :")) # hobby input 
+print("hello my name is "+name+" i am " +age+" years old and my faviourate hobby is "+hobby+"ッ")
+print(hobby.lower())     #convert the str into lower letter.
+print(hobby.capitalize()) #convert the str into capital letter.
+print(hobby.__len__())    #tell the number character of the str.
